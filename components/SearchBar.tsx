2f8b5dc9181b8bb8bea48f1547098915d49b33e5
Kristian <10221425@ucn.dk>
2024-02-25 13:29:51 +0100

    Car Catalogue Search

---
+++ components/SearchBar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+// import { useRouter } from "next/navigation";
+
+import SearchManufacturer from "./SearchManufacturer";
+
+const SearchBar = () => {
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    // e.preventDefault();
+
+    // if (manufacturer.trim() === "" && model.trim() === "") {
+    //   return alert("Please provide some input");
+    // }
+
+    // updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+    <div className='searchbar__item'>
+      <SearchManufacturer
+        manufacturer={manufacturer}
+        setManuFacturer={setManuFacturer}
+      />
+      {/* <SearchButton otherClasses='sm:hidden' /> */}
+    </div>
+    <div className='searchbar__item'>
+      <Image
+        src='/model-icon.png'
+        width={25}
+        height={25}
+        className='absolute w-[20px] h-[20px] ml-4'
+        alt='car model'
+      />
+      <input
+        type='text'
+        name='model'
+        value={model}
+        onChange={(e) => setModel(e.target.value)}
+        placeholder='Tiguan...'
+        className='searchbar__input'
+      />
+      {/* <SearchButton otherClasses='sm:hidden' /> */}
+    </div>
+    {/* <SearchButton otherClasses='max-sm:hidden' /> */}
+  </form>
+  )
+}
+export default SearchBar
