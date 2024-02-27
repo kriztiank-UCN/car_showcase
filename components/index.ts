9c84dbb18be7e71a2b44854de88873f83a58f455
Kristian <10221425@ucn.dk>
2024-02-27 15:51:24 +0100

    Show More Button

--- components/index.ts
+++ components/index.ts
@@ -1,12 +1,13 @@
-import CarCard from "./CarCard";
-import CarDetails from "./CarDetails";
+import Hero from './Hero'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
 import CustomButton from './CustomButton'
 import CustomFilter from './CustomFilter'
 import Footer from './Footer'
 import NavBar from './Navbar'
 import SearchBar from './SearchBar'
 import SearchManufacturer from './SearchManufacturer'
-import Hero from './Hero'
+import ShowMore from './ShowMore'
 
 export {
   Hero,
@@ -18,4 +19,5 @@ export {
   NavBar,
   SearchBar,
   SearchManufacturer,
+  ShowMore,
 }
